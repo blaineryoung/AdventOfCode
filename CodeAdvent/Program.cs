ed5ab4b29618b0d8919162e1c0a3200a1b15b282
Blaine Young <blaineryoung@hotmail.com>
2021-11-30 22:51:44 -0800

    First challenge

--- CodeAdvent/Program.cs
+++ CodeAdvent/Program.cs
@@ -1,3 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
-Console.WriteLine("Hello, World!");
+namespace CodeAdvent // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int increases = CountIncreases("input.txt");
+            Console.WriteLine($"Total is {increases}");
+        }
+
+        public static int CountIncreases(String inputFile)
+        {
+            int? lastSeen = null;
+            int count = 0;
+
+            foreach (string line in System.IO.File.ReadLines(inputFile))
+            {
+                int current = 0;
+                if (false == int.TryParse(line, out current))
+                {
+                    throw new Exception($"Bad input {line}");
+                }
+
+                if (null != lastSeen && lastSeen < current)
+                {
+                    count++;
+                }
+
+                lastSeen = current;
+            }
+
+            return count;
+        }
+    }
+}
