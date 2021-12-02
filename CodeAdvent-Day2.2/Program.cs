3f96e54de0b22c1175a930a7b9a51f24044543bc
Blaine Young <blaineryoung@hotmail.com>
2021-12-01 21:45:36 -0800

    Day 2 stuff

---
+++ CodeAdvent-Day2.2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace CodeAdvent // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+        // sliding window
+
+        public static void Main(string[] args)
+        {
+            int increases = TrackPosition("input.txt");
+            Console.WriteLine($"Total is {increases}");
+        }
+
+        public static int TrackPosition(string inputFile)
+        {
+            int horizontal = 0;
+            int depth = 0;
+            int aim = 0;
+
+            // tuple is horizontal, aim change
+            Dictionary<string, Tuple<int, int>> directions = new Dictionary<string, Tuple<int, int>>()
+            {
+                { "forward", new Tuple<int, int>(1, 0) },
+                { "down", new Tuple<int, int>(0, 1) },
+                { "up" , new Tuple<int, int>(0, -1) },
+            };
+
+            foreach (string line in System.IO.File.ReadLines(inputFile))
+            {
+                String[] tokens = line.Split(' ');
+                Tuple<int, int> direction = directions[tokens[0]];
+                horizontal = horizontal + (direction.Item1 * int.Parse(tokens[1]));
+                depth = depth + (direction.Item1 * aim * int.Parse(tokens[1]));
+
+                aim = aim + (direction.Item2 * int.Parse(tokens[1]));
+            }
+
+            return horizontal * depth;
+        }
+    }
+}
